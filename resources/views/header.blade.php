6111acb247ab978632ac332888d32d70d32f6113
hawary-id <hawaryproject@gmail.com>
2021-09-23 11:19:15 +0700

    revisi title header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>QC | Calibration Division</title>
+        <title>Quality Control | Calibration Division</title>
         <link rel="icon" href="{{url('/images/logo.png')}}" type="image/x-icon"/>
         <!-- Bootstrap 5 CSS-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
